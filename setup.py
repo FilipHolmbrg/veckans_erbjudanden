faac3ca3943fcbd25bc9ccddf024470ef6a6f95d
FilipHolmbrg <148468773+FilipHolmbrg@users.noreply.github.com>
2024-09-24 10:14:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='veckans_mat',             # The name of your package
-    version='1.0.0',                # Version number of your package
-    packages=find_packages(),       # Automatically finds all packages
-    install_requires=[              # List your dependencies here
-        # 'numpy',                  # Example dependencies
-        # 'requests',
-    ],
+    name='veckans_erbjudanden',
+    version='0.1',
+    packages=find_packages(),  # Automatically finds all packages (including veckans_mat)
+    install_requires=[],
     author='Filip Holmberg',
-    author_email='filip.holmberg.se@gmail.com',
-    description='Package to get offers and match with recipes',
-    url='https://github.com/FilipHolmbrg/veckans_erbjudanden',  # GitHub URL
-    classifiers=[
-        'Programming Language :: Python :: 3',
-    ],
+    description='A package for veckans erbjudanden',
+    url='https://github.com/FilipHolmbrg/veckans_erbjudanden',
     python_requires='>=3.6',
 )
